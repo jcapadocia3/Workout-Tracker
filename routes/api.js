fda34c6081f530e19f37fe968d32303630ca93cd
James Capadocia <james.capadocia@gmail.com>
2021-12-02 15:29:45 -0600

    Coded GET route to pull data saved in DB and show in the client on root route.

--- routes/api.js
+++ routes/api.js
@@ -28,4 +28,19 @@ router.put("/api/workouts/:id", (req, res) => {
       });
   });
 
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+  .then(data => {
+    data.forEach(workout => {
+      var total = 0;
+      workout.exercises.forEach(e => {
+        total += e.duration;
+      });
+      workout.totalDuration = total;
+    });
+    res.json(data);
+  }).catch(err => {
+    res.json(err);
+  })
+}) 
 module.exports = router;
