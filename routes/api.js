b5a84084dd771873ec8886adea4e232b2ef534b3
James Capadocia <james.capadocia@gmail.com>
2021-12-02 15:55:04 -0600

    Created another GET route to 'find all' workouts completed to display the data in the 'dashboard'

--- routes/api.js
+++ routes/api.js
@@ -12,35 +12,47 @@ router.post("/api/workouts", ({ body }, res) => {
 });
 
 router.put("/api/workouts/:id", (req, res) => {
-    db.Workout.findOneAndUpdate(
-      { _id: req.params.id },
-      {
-        $inc: { totalDuration: req.body.duration },
-        $push: { exercises: req.body },
-      },
-      { new: true }
-    )
-      .then((data) => {
-        res.json(data);
-      })
-      .catch((err) => {
-        res.json(err);
-      });
-  });
+  db.Workout.findOneAndUpdate(
+    { _id: req.params.id },
+    {
+      $inc: { totalDuration: req.body.duration },
+      $push: { exercises: req.body },
+    },
+    { new: true }
+  )
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 router.get("/api/workouts", (req, res) => {
   db.Workout.find({})
-  .then(data => {
-    data.forEach(workout => {
-      var total = 0;
-      workout.exercises.forEach(e => {
-        total += e.duration;
+    .then((data) => {
+      data.forEach((workout) => {
+        var total = 0;
+        workout.exercises.forEach((e) => {
+          total += e.duration;
+        });
+        workout.totalDuration = total;
       });
-      workout.totalDuration = total;
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json(err);
     });
-    res.json(data);
-  }).catch(err => {
-    res.json(err);
-  })
-}) 
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
 module.exports = router;
