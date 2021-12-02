6278fb690eb5ee959bb9947856285dc5a3241c74
James Capadocia <james.capadocia@gmail.com>
2021-12-01 21:55:27 -0600

    Coded PUT route. Object created from POST route now populates all data into the DB based on object ID. Still nothing showing on client aside from confirmation that data was saved.

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,29 @@ const router = require("express").Router();
 
 router.post("/api/workouts", ({ body }, res) => {
   db.Workout.create(body)
-  .then(data => {
-    res.json(data);
-  }).catch(err => {
-    res.json(err)
-  });
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
+
+router.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        $inc: { totalDuration: req.body.duration },
+        $push: { exercises: req.body },
+      },
+      { new: true }
+    )
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 
 module.exports = router;
